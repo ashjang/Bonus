d6d565c7b3a4e43e56e8a9741e91a9d741dbb02f
ashjang <vkdvkdgkfla@naver.com>
2023-05-02 16:42:12 +0900

    modify BonusApplication.java

--- src/main/java/com/example/bonus/BonusApplication.java
+++ src/main/java/com/example/bonus/BonusApplication.java
@@ -5,6 +5,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
