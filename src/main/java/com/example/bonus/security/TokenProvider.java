16df37a5f493427619c9303a2a20d7dd2071d97c
ashjang <vkdvkdgkfla@naver.com>
2023-05-07 16:44:12 +0900

    add generateToken()

--- src/main/java/com/example/bonus/security/TokenProvider.java
+++ src/main/java/com/example/bonus/security/TokenProvider.java
@@ -1,4 +1,45 @@
 package com.example.bonus.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60;       // 한 시간
+
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    /**
+     * 토큰 생성(발급)
+     * @param username
+     * @param roles
+     * @return
+     */
+
+    public String generateToken(String username, List<String> roles) {
+        // 사용자 권한 정보 저장하는 claim
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        var now = new Date();
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)           // 토큰 생성 시간
+                .setExpiration(expiredDate) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS512, this.secretKey)    // 사용할 알고리즘, 비밀키
+                .compact();
+    }
 }
