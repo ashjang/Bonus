33792a4e5fc4378155661c4978a830cfe9ce28fc
ashjang <vkdvkdgkfla@naver.com>
2023-05-07 16:52:48 +0900

    parsing Token

--- src/main/java/com/example/bonus/security/TokenProvider.java
+++ src/main/java/com/example/bonus/security/TokenProvider.java
@@ -1,11 +1,13 @@
 package com.example.bonus.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.List;
@@ -41,5 +43,29 @@ public class TokenProvider {
                 .setExpiration(expiredDate) // 토큰 만료 시간
                 .signWith(SignatureAlgorithm.HS512, this.secretKey)    // 사용할 알고리즘, 비밀키
                 .compact();
+    }
+
+    // 토큰으로부터 username 받기
+    public String getUsername(String token) {
+        return this.parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        // token이 빈 값일 때
+        if (!StringUtils.hasText(token)) return false;
+
+        var claims = this.parseClaims(token);
+        // 현재 시간과 비교
+        return claims.getExpiration().before(new Date());
+    }
+
+    // 토큰으로부터 claim을 가져오는
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(this.secretKey).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+
     }
 }
